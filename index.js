af0e3fc681d5a478abcda98356ceb8c3f50d8ea5
PactForge <pactforge@gmail.com>
2025-05-17 21:14:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-```javascript
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -53,4 +52,3 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log('Server running on port ' + PORT);
 });
-```
