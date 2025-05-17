8ce658e892ff68056924d427693e982744f7e9da
PactForge <pactforge@gmail.com>
2025-05-17 20:22:24 +0530

    Update Post.js

--- models/Post.js
+++ models/Post.js
@@ -1,9 +1,22 @@
+```javascript
 const mongoose = require('mongoose');
 
 const postSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  content: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now }
-});
+  content: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  comments: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Comment',
+  }],
+}, { timestamps: true });
 
 module.exports = mongoose.model('Post', postSchema);
+```
